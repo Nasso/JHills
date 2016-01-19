91c27b0ca7a554c1621dee8a5e8f872b6387f9cc
Nasso <nassomails@gmail.com>
2016-01-19 22:25:20 +0100

    Map preview when typing seed

--- src/org/nasso/jhills/MenuLevel.java
+++ src/org/nasso/jhills/MenuLevel.java
@@ -14,6 +14,14 @@ import org.nasso.engine.KeyInfo;
 import org.nasso.engine.Level;
 
 public class MenuLevel extends Level {
+	private Map previewMap = new Map(100, 8, 2.5f, 0);
+	private boolean mapUpdate = false;
+	
+	private float lastSeedModification = 0;
+	private float theNow = 0;
+	
+	private long lastSeed = 0;
+	
 	private Callback onValidation;
 	private String seed = "";
 	
@@ -29,6 +37,24 @@ public class MenuLevel extends Level {
 		gtx.setFill(Color.web("#784800"));
 		gtx.fillRect(0, 0, this.getWidth(), this.getHeight());
 
+		gtx.save(); // MAP_RENDER
+			gtx.beginPath();
+				for(int i = 0; i < previewMap.getPreciseHeights().length; i++){
+					float x = i * previewMap.getPreciseXSpace() * 100;
+					float y = this.getHeight() - previewMap.getPreciseHeightAt(i) * 100;
+					
+					gtx.lineTo(x, y);
+				}
+				
+				gtx.stroke();
+				
+				gtx.lineTo(previewMap.getPreciseHeights().length * previewMap.getPreciseXSpace() * 100, 0);
+				gtx.lineTo(0, 0);
+				
+				gtx.fill();
+			gtx.closePath();
+		gtx.restore(); // !MAP_RENDER
+		
 		gtx.setFill(Color.web("#FFFFFF"));
 		gtx.setTextAlign(TextAlignment.CENTER);
 		gtx.fillText("Map seed:", this.getWidth()/2, this.getHeight()/2);
@@ -37,7 +63,7 @@ public class MenuLevel extends Level {
 		float textBoxHeight = 40;
 		
 		gtx.fillRoundRect(this.getWidth()/2 - textBoxWidth/2, this.getHeight()/2 + 16, textBoxWidth, textBoxHeight, 8, 8);
-		
+
 		gtx.save(); // TEXT_BOX_CLIP
 			gtx.beginPath();
 				gtx.rect(this.getWidth()/2 - textBoxWidth/2, this.getHeight()/2 + 16, textBoxWidth, textBoxHeight);
@@ -59,14 +85,18 @@ public class MenuLevel extends Level {
 	public void keyDown(KeyInfo key) {
 		if(key.getKeyCode() == KeyCode.BACK_SPACE && seed.length() > 0){
 			seed = seed.substring(0, seed.length()-1);
+			lastSeedModification = theNow;
+			mapUpdate = false;
 		}else if(key.getKeyCode() == KeyCode.ENTER){
 			onValidation.call();
 		}
 	}
 	
 	public void keyType(KeyInfo key) {
-		if(key.getKeyChar() != KeyEvent.CHAR_UNDEFINED && key.getKeyChar().matches("[a-zA-Z0-9]") && seed.length() < 30){
+		if(key.getKeyChar() != KeyEvent.CHAR_UNDEFINED && key.getKeyChar().matches("[a-zA-Z0-9\\s]*") && seed.length() < 30){
 			seed += key.getKeyChar();
+			lastSeedModification = theNow;
+			mapUpdate = false;
 		}
 	}
 	
@@ -79,10 +109,20 @@ public class MenuLevel extends Level {
 	}
 	
 	public void step(float delta, float now) {
+		theNow = now;
 		
+		if(theNow - lastSeedModification >= 2000 && !mapUpdate){
+			mapUpdate = true;
+			lastSeed = (seed.length() == 0 ? new Random().nextLong() : seed.hashCode());
+			new Thread(new Runnable(){
+				public void run() {
+					previewMap = new Map(100, 8, 2.5f, lastSeed);
+				}
+			}).start();
+		}
 	}
 	
 	public long getSeed(){
-		return (seed.length() == 0 ? new Random().nextLong() : seed.hashCode());
+		return lastSeed;
 	}
 }
